fa95cfd984359327e5523e5d8ede51471885d022
wagnerSfarias <wagner@universalhost.com.br>
2024-08-14 19:37:12 -0300

    feat : creating button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from 'react'
+
+import { Container } from './styles'
+
+type ButtonProps = ComponentProps<'button'> & {
+  variant?: 'default' | 'outline'
+}
+
+export function Button({
+  children,
+  variant = 'default',
+  ...props
+}: ButtonProps) {
+  return (
+    <Container {...props} $variant={variant}>
+      {children}
+    </Container>
+  )
+}
