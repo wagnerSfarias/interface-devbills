f1d225770994bff94a3264e005a7bbb3bd68e6e1
wagnerSfarias <wagner@universalhost.com.br>
2024-08-14 19:38:21 -0300

    feat : inserting buttons

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,10 +1,22 @@
+import { Button } from '../../components/Button'
 import { Logo } from '../../components/Logo'
-import { Header } from './style'
+import { Header, Main, Section, Filters } from './style'
 
 export function Home() {
   return (
-    <Header>
-      <Logo />
-    </Header>
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button>Nova Transação</Button>
+          <Button>Nova Categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters></Filters>
+        </Section>
+      </Main>
+    </>
   )
 }
