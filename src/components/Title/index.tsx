8f033eeae14273a3055f361141276ab73c6ccb4e
wagnerSfarias <wagner@universalhost.com.br>
2024-08-15 23:09:40 -0300

    feat : creating button component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from './styles'
+
+type TitleProps = {
+  title: string
+  subtitle: string
+}
+export function Title({ title, subtitle }: TitleProps) {
+  return (
+    <Container>
+      <h2>{title}</h2>
+      <span>{subtitle}</span>
+    </Container>
+  )
+}
