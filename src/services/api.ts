dc0b449edc21402c7151343b11ea23a902470f38
wagnerSfarias <wagner@universalhost.com.br>
2024-09-03 23:50:59 -0300

    feat: creating calls to category route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,25 @@
 import axios from 'axios'
 
+import { Category, CreateCategory } from './api-types'
+
 export class APIService {
   private static client = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
   })
+
+  static async createCategory(
+    cretaCategoryData: CreateCategory,
+  ): Promise<Category> {
+    const { data } = await APIService.client.post<Category>(
+      '/categories',
+      cretaCategoryData,
+    )
+    return data
+  }
+
+  static async getCategories(): Promise<Category[]> {
+    const { data } = await APIService.client.get<Category[]>('/categories')
+
+    return data
+  }
 }
