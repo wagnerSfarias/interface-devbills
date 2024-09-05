9adbe6f5e06fbc959f8361d7b0d534ae0a9c597d
wagnerSfarias <wagner@universalhost.com.br>
2024-09-05 15:28:32 -0300

    fix: add context provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
+import { AppProvider } from './hooks'
 import { Home } from './screens/Home/home'
 import GlobalStyles from './styles/global'
 
 export function App() {
   return (
-    <>
+    <AppProvider>
       <GlobalStyles />
       <Home />
-    </>
+    </AppProvider>
   )
 }
